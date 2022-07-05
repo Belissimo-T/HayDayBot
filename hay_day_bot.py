612a8ba6833292b97bd58fac4f73d1ea8f94860b
Bela Tollkien <bela.tollking@gmail.com>
2022-07-06 00:43:00 +0200

    hay day ads

--- hay_day_bot.py
+++ hay_day_bot.py
@@ -1,4 +1,6 @@
+import dataclasses
 import time
+from enum import Enum
 
 import PIL.Image
 import cv2
@@ -36,7 +38,7 @@ class UIController:
         # img.save("img.png")
         return img
 
-    def locate_image(self, image: str, tries=10, confidence=0.9):
+    def locate_image(self, image: str, tries=10, confidence=0.85):
         x, y, x2, y2 = self.locate_image_bbox(image, tries, confidence)
 
         dx = x2 - x
@@ -44,7 +46,7 @@ class UIController:
 
         return x + dx / 2, y + dy / 2
 
-    def locate_image_bbox(self, image: str, tries=10, confidence=0.9):
+    def locate_image_bbox(self, image: str, tries=10, confidence=0.85):
         image_path = os.path.join("images", image)
 
         common_width = 1600
@@ -152,6 +154,99 @@ class UIController:
         return self.ui_stack[-1]
 
 
+@dataclasses.dataclass
+class HayDayItem:
+    name: str
+    default_price: int
+
+    def default_price_at(self, quantity: int) -> int:
+        return int(self.default_price * quantity)
+
+    def maximum_price_at(self, quantity: int) -> int:
+        return int(self.default_price * quantity * 3.6)
+
+
+class HayDayItems(Enum):
+    wheat = HayDayItem("Wheat", 1)
+    corn = HayDayItem("Corn", 2)
+    carrot = HayDayItem("Carrot", 2)
+    soybean = HayDayItem("Soybean", 3)
+    indigo = HayDayItem("Indigo", 7)
+    cotton = HayDayItem("Cotton", 8)
+    pumpkin = HayDayItem("Pumpkin", 9)
+    chili_pepper = HayDayItem("Chili Pepper", 10)
+    apples = HayDayItem("Apples", 11)
+    raspberries = HayDayItem("Raspberries", 13)
+    cherries = HayDayItem("Cherries", 19)
+    blackberries = HayDayItem("Blackberries", 23)
+
+    dynamite = HayDayItem("Dynamite", 7)
+    axe = HayDayItem("Axe", 10)
+    saw = HayDayItem("Saw", 15)
+    tnt_barrel = HayDayItem("TNT Barrel", 20)
+    shovel = HayDayItem("Shovel", 30)
+    plank = HayDayItem("Plank", 75)
+    nail = HayDayItem("Nail", 75)
+    bolt = HayDayItem("Bolt", 75)
+    screw = HayDayItem("Screw", 75)
+    duct_tape = HayDayItem("Duct Tape", 75)
+    wood_panel = HayDayItem("Wood Panel", 75)
+    land_deed = HayDayItem("Land Deed", 112)
+    mallet = HayDayItem("Mallet", 112)
+    marker_stake = HayDayItem("Marker Stake", 112)
+
+    sheep_feed = HayDayItem("Sheep Feed", 4)
+    silver_ore = HayDayItem("Silver Ore", 5)
+    gold_ore = HayDayItem("Gold Ore", 6)
+    popcorn = HayDayItem("Popcorn", 9)
+    white_sugar = HayDayItem("White Sugar", 14)
+    cream = HayDayItem("Cream", 14)
+    wool = HayDayItem("Wool", 15)
+    corn_bread = HayDayItem("Corn Bread", 20)
+    carrot_pie = HayDayItem("Carrot Pie", 23)
+    butter = HayDayItem("Butter", 23)
+    pancake = HayDayItem("Pancake", 30)
+    cotton_fabric = HayDayItem("Cotton Fabric", 30)
+    blue_woolly_hat = HayDayItem("Blue Woolly Hat", 31)
+    chili_popcorn = HayDayItem("Chili Popcorn", 34)
+    cheese = HayDayItem("Cheese", 34)
+    buttered_popcorn = HayDayItem("Buttered Popcorn", 35)
+    raspberry_muffin = HayDayItem("Raspberry Muffin", 39)
+    silver_bar = HayDayItem("Silver Bar", 41)
+    sweater = HayDayItem("Sweater", 42)
+    pumpkin_pie = HayDayItem("Pumpkin Pie", 44)
+    carrot_cake = HayDayItem("Carrot Cake", 46)
+    syrup = HayDayItem("Syrup", 50)
+    hamburger = HayDayItem("Hamburger", 50)
+    gold_bar = HayDayItem("Gold Bar", 50)
+    bacon_and_eggs = HayDayItem("Bacon and Eggs", 56)
+    platinum_bar = HayDayItem("Platinum Bar", 57)
+    blue_sweater = HayDayItem("Blue Sweater", 58)
+    cream_cake = HayDayItem("Cream Cake", 61)
+    bacon_pie = HayDayItem("Bacon Pie", 61)
+    blackberry_muffin = HayDayItem("Blackberry Muffin", 63)
+    red_berry_cake = HayDayItem("Red Berry Cake", 71)
+    apple_pie = HayDayItem("Apple Pie", 75)
+    cheesecake = HayDayItem("Cheesecake", 79)
+    wooly_chaps = HayDayItem("Wooly Chaps", 86)
+    violet_dress = HayDayItem("Violet Dress", 91)
+
+
+@dataclasses.dataclass
+class HayDayAd:
+    item: HayDayItem
+    quantity: int
+    price: int
+
+    @property
+    def price_delta_to_maximum(self):
+        price_delta = self.item.maximum_price_at(self.quantity) - self.price
+
+        assert price_delta >= 0
+
+        return price_delta
+
+
 class HayDayController(UIController):
     def list_advertisements(self):
         self.reset()
@@ -171,28 +266,35 @@ class HayDayController(UIController):
         pyautogui.dragTo(*self.ratio_coords_to_absolute(0.7, 0.5), .3, button='left')
 
     def get_current_newspaper_page(self):
+        assert self.current_ui_state == "newspaper"
+
         # find number by searching for 2 with low confidence
 
         x, y = self.locate_image("newspaper_two.png", tries=1, confidence=0.5)
 
-        return int(self.get_text_at(
-            (x - .02, y - .015, x + .02, y + 0.02),
-            custom_config="--psm 8 -c tessedit_char_whitelist=0123456789",
-            scale_factor=10,
-            threshold=126,
-            dilation=8
-        ))
+        try:
+            return int(self.get_text_at(
+                (x - .02, y - .015, x + .02, y + 0.02),
+                custom_config="--psm 8 -c tessedit_char_whitelist=0123456789",
+                scale_factor=10,
+                threshold=126,
+                dilation=8
+            ))
+        except Exception as e:
+            raise FailedNavigationException(f"Failed to get current newspaper page.") from e
 
     def change_newspaper_page(self, to: int = 2):
+        assert self.current_ui_state == "newspaper"
+
         to = to // 2 * 2
 
         assert 2 <= to <= 18, f"Invalid page number: {to}"
 
         failures = -1
 
-        while (curr := self.get_current_newspaper_page()) != to:
+        while (curr := self.get_current_newspaper_page()) != to and failures < 5:
             failures += 1
-            print(f"Measuring page: {curr}, {failures=}")
+            print(f" -> Measured page: {curr}, {failures=}")
 
             while curr != to:
                 if curr < to:
@@ -202,9 +304,14 @@ class HayDayController(UIController):
                     self.scroll_left()
                     curr -= 2
 
-                print(f"Current Newspaper nr: {curr} desired: {to}")
+                print(f" -> Current Newspaper nr: {curr} desired: {to}")
 
             time.sleep(2)
+
+    def get_current_scrolled_newspaper_ads(self):
+        assert self.current_ui_state == "newspaper"
+
+        ads = []
 
 
 if __name__ == "__main__":
@@ -218,7 +325,7 @@ if __name__ == "__main__":
     #
     # # print(controller.locate_image_bbox("newspaper_two.png"))
 
-    print(f"{controller.get_current_newspaper_page()!r}")
+    # print(f"{controller.get_current_newspaper_page()!r}")
 
     controller.change_newspaper_page(7)
     controller.change_newspaper_page(18)
